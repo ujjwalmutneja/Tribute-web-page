0c7f9d51c15204be15930e3e2ec808f6526ae7c0
ujjwalmutneja <122460967+ujjwalmutneja@users.noreply.github.com>
2023-05-16 10:36:12 +0530

    Add files via upload
    This project is tribute web page to virat kohli using html Css and java script

---
+++ project2/script.js
@@ -0,0 +1,9 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    // Code to execute after the page has finished loading
+    var button = document.querySelector('#myButton');
+    button.addEventListener('click', function() {
+      alert('Button clicked!');
+    });
+  });
+  
